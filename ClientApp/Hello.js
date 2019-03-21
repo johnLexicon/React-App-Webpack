ddc5db99452ae53fca0d7bddab1deccd2cc77562
John Lundgren <john.paul.lundgren@gmail.com>
2019-03-21 11:51:55 +0100

    Webpack config for react app.

---
+++ ClientApp/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => {
+  return <p>Hello world from React Stateless Function Component</p>;
+};
+
+export default Hello;
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const webpack = require("webpack");
 const path = require("path");
 
 // /Users/johnlundgren/dev/reactJs/react-app-webpack/ClientApp
-const APP_DIR = path.resolve(__dirname, "src");
+const APP_DIR = path.resolve(__dirname, "ClientApp");
 // /Users/johnlundgren/dev/reactJs/react-app-webpack/public
 const PUBLIC_DIR = path.resolve(__dirname, "public");
 
 //Object with instructions for Webpack to know what to do.
 const config = {
-  entry: path.resolve(APP_DIR, "helloworld.js"), //Entry is what should be bundled.
+  entry: path.resolve(APP_DIR, "Client.js"), //Entry is what should be bundled.
   devServer: {
     contentBase: PUBLIC_DIR, //Webserver's base directory
     port: 9000, //Default port
